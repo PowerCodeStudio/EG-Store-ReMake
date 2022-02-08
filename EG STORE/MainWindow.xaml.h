953c0cee7d0a7419a4cb6975f2e60a1a7cb9ebdf
CodeZhangBorui <zhangborui.2008@foxmail.com>
2022-02-08 20:46:54 +0800

    Project Created

---
+++ EG STORE/MainWindow.xaml.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::EG_STORE::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::EG_STORE::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
